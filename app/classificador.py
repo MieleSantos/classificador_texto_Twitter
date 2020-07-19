abcec9c5c0365c6e9a65ad60815bfa78de5d8f41
mielesantos <mielesnts@gmail.com>
2020-07-19 20:56:42 -0300

    add projeto

---
+++ app/classificador.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+import  spacy
+import string
+import re
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+
+    predictin =''
+    if request.method == 'POST':
+
+        query = request.form['query']    
+        if query:
+            sentimento = process_model(query)
+            return render_template('index.html',sentimento=sentimento)
+    else:
+        return render_template('index.html' )
+
+def process_model(texto):
+
+    text_previsao = prepocessamento(texto)
+
+    modelo = spacy.load('modelos')
+
+    result_previssao = modelo(text_previsao).cats
+    
+    return result_previssao
+
+def prepocessamento(texto):
+
+    pln = spacy.load('pt')
+    stop_words = spacy.lang.pt.stop_words.STOP_WORDS
+    
+    #letras maiuscula
+    texto = texto.lower()
+    #remocao do nome usuario
+    texto = re.sub(r"@[A-Za-z0-9$-_@.&+]+",' ', texto)
+    #removemndo as urls
+    texto = re.sub(r"https?://[A-Za-z0-9./]+",' ', texto)
+    #removendo espacos em branco
+    texto = re.sub(r" +",' ', texto)
+    #removendo emoticons
+    lista_emocoes = {
+        ':)':'emocaopositiva',
+        ':d':'emocaopositiva',
+        ':(':'emocaonegativa'
+        }
+    for emocao in lista_emocoes:
+        texto = texto.replace(emocao, lista_emocoes[emocao])
+    
+    #lematizacao
+    documento = pln(texto)
+    lista = []
+    for token in documento:
+        lista.append(token.lemma_)
+
+    #removendo stop_words e pontuacao
+    lista = [palavra for palavra in lista if palavra not in stop_words and palavra not in string.punctuation]
+    lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+
+    return lista
+
+if __name__ == "__main__":
+    app.run(debug = True)
